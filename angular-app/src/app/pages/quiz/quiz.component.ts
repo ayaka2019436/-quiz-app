8cd58150c9d1b85ed7bd21031e9d9f7b06554cb9
薄井彩花 <>
2023-06-05 12:00:44 +0900

    クイズ画面を作成（ボタンの配置悩み中）

--- angular-app/src/app/pages/quiz/quiz.component.ts
+++ angular-app/src/app/pages/quiz/quiz.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-quiz',
   templateUrl: './quiz.component.html',
-  styleUrls: ['./quiz.component.scss']
+  styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent {
-
+  Questionscount: number = 0;
+  quizQuestion: string = 'クイズの問題を表示させる';
 }
