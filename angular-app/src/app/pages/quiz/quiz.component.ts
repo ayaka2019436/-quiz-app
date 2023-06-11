f3f33b6657b3c5f9633339c5d3f0c7c54410262c
薄井彩花 <>
2023-06-12 08:59:52 +0900

    フォントの変更

--- angular-app/src/app/pages/quiz/quiz.component.ts
+++ angular-app/src/app/pages/quiz/quiz.component.ts
@@ -14,20 +14,24 @@ export class QuizComponent {
   answer_num: number = 2;
   answer: string = 'クイズの回答';
   description: string = 'クイズの解説';
+  selectedOptionIndex: number = -1;
+  your_answer: string = '';
 
   handleOptionClick(optionIndex: number) {
     // ボタンがクリックされた時の処理を実装
     //boolean型に選択済みであればtrueを代入し、disabledで選択を無効化する
-    if (this.isOptionSelected[optionIndex] == true) {
-      console.log('選択肢', optionIndex + 1, 'は既に選択済みです。');
+    if (this.selectedOptionIndex !== -1) {
+      console.log('既に選択済みの選択肢があります。');
+      return;
     } else {
       console.log('選択肢', optionIndex + 1, 'が選択されました');
-      this.isOptionSelected[optionIndex] = true;
-      if (this.answer_num === optionIndex + 1) {
-        this.circle_cross = '◯';
-      } else {
-        this.circle_cross = '✖︎';
-      }
+      this.selectedOptionIndex = 1;
+    }
+
+    if (this.answer_num === optionIndex + 1) {
+      this.circle_cross = '◯';
+    } else {
+      this.circle_cross = '✖︎';
     }
   }
 }
