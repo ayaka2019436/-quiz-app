a8914461d307e6bc5c339f9bdf77e6c694e7e934
薄井彩花 <>
2023-07-19 11:20:58 +0900

    正誤と解説を表示できる

--- angular-app/src/app/pages/quiz/quiz.component.ts
+++ angular-app/src/app/pages/quiz/quiz.component.ts
@@ -8,11 +8,11 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent {
-  Questionscount: number = 1;
+  questionsCount: number = 1;
   quizQuestion: string = '';
   //isOptionSelected: boolean[] = [false, false, false, false];
   circle_cross: string = '';
-  answer_num: number = 2;
+  answer_num: number = 0;
   answer: string = 'クイズの回答';
   description: string = 'クイズの解説';
   selectedOptionIndex: number = -1;
@@ -23,12 +23,20 @@ export class QuizComponent {
     this.apiSvc.getQuizzes(query).subscribe((quizzes) => {
       // 確認用後で消しときます
       console.log(quizzes);
-      // for (let i in quizzes.length) {
-      this.quizQuestion = quizzes.data[0].attributes.question;
+      this.quizQuestion =
+        quizzes.data[this.questionsCount - 1].attributes.question;
       for (let index in this.quizChoice) {
-        this.quizChoice[index] = quizzes.data[0].attributes.choices[index].text;
+        this.quizChoice[index] =
+          quizzes.data[this.questionsCount - 1].attributes.choices[index].text;
+        if (
+          true ===
+          quizzes.data[this.questionsCount - 1].attributes.choices[index]
+            .is_correct
+        ) {
+          this.answer_num =
+            quizzes.data[this.questionsCount - 1].attributes.choices[index].id;
+        }
       }
-      //}
     });
   }
 
@@ -43,10 +51,21 @@ export class QuizComponent {
       this.selectedOptionIndex = 1;
     }
 
-    if (this.answer_num === optionIndex + 1) {
-      this.circle_cross = '◯';
-    } else {
-      this.circle_cross = '✖︎';
-    }
+    const query: any = { populate: ['choices'] };
+    this.apiSvc.getQuizzes(query).subscribe((quizzes) => {
+      this.description =
+        quizzes.data[this.questionsCount - 1].attributes.explanation;
+      this.answer =
+        quizzes.data[this.questionsCount - 1].attributes.choices[
+          this.answer_num
+        ].text;
+      if (
+        this.answer_num === quizzes.data[0].attributes.choices[optionIndex].id
+      ) {
+        this.circle_cross = '◯';
+      } else {
+        this.circle_cross = '✖︎';
+      }
+    });
   }
 }
